d3c085da57bb5296fe3f46bb3c3aac2a6931f626
Ilya <pincetgor@mail.ru>
2021-07-03 17:21:50 +0300

    2nd task done

--- main.cpp
+++ main.cpp
@@ -3,16 +3,22 @@
 
 using namespace std;
 
+#define RANGE(x,y) ((x)>=0 && (x)<(y)) ? (cout << "true" << endl) : (cout << "False" << endl)
+
 int main()
 {
-    int positive = 0;
-    int negative = 0;
-    int size;
-    cout << "Enter array size: ";
-    cin >> size;
-    float arr[size];
+//    //Task 1
+//    int positive = 0;
+//    int negative = 0;
+//    int size;
+//    cout << "Enter array size: ";
+//    cin >> size;
+//    float arr[size];
+//    arrReg(arr, size);
+//    printArray(arr,size);
+//    arrSort(arr,size,positive,negative);
 
-    arrReg(arr, size);
-    printArray(arr,size);
-    arrSort(arr,size,positive,negative);
+    //Task 2
+    RANGE (9,10);
+
 }
